bbd0ac7776a124665e7f0382c2fb368fcab650c3
kinyuykelly <kinyuykelly213@gmail.com>
2024-04-02 13:10:51 +0100

    added animated weather icons

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ export default function App() {
     <div className="App">
       <div className='container'>
       <Weather defaultcity='New York'/>
-    <footer>Coded by {""} <a href='https://github.com/Kinyuykelly' target='_blank'> Kinyuy Kelly</a> {""} and open-sourced on {""}
-     <a href='https://github.com/Kinyuykelly/react-weather-app' target='_blank'> {""}GitHub</a></footer>
+    <footer>Coded by {""} <a href='https://github.com/Kinyuykelly' target='_blank' rel="noreferrer"> Kinyuy Kelly</a> {""} and open-sourced on {""}
+     <a href='https://github.com/Kinyuykelly/react-weather-app' target='_blank' rel="noreferrer"> {""}GitHub</a></footer>
       </div>
     
     </div>
